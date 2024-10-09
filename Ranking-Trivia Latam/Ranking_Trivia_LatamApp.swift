08225d9aeab058629ff20f46e1b61767a813ffca
Arturo Gomez <arturo.gomez@nextiva.com>
2024-10-09 15:25:53 -0600

    feat: first commit setting up the project

---
+++ Ranking-Trivia Latam/Ranking_Trivia_LatamApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ranking_Trivia_LatamApp.swift
+//  Ranking-Trivia Latam
+//
+//  Created by Arturo Gomez on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Ranking_Trivia_LatamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ranking-Trivia LatamTests/Ranking_Trivia_LatamTests.swift
@@ -0,0 +1,17 @@
+//
+//  Ranking_Trivia_LatamTests.swift
+//  Ranking-Trivia LatamTests
+//
+//  Created by Arturo Gomez on 10/9/24.
+//
+
+import Testing
+@testable import Ranking_Trivia_Latam
+
+struct Ranking_Trivia_LatamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
